836abae5c74525fccb22cd11f509f51ff79c8dc0
Anastasiia Kulachok kulachokn@gmail.com <kulachokn@gmail.com>
2025-01-05 14:35:43 +0100

    task 5

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,11 @@
-// Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+
+const User = {
+  [username]: username,
+  [password]: password,
+  age: 111,
+};
+
+console.log(User.username);
+console.log(User.password);
