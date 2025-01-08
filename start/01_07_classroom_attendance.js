f22076cdd5c27bd6dfbf02c5ea3640e930ee1975
Anastasiia Kulachok kulachokn@gmail.com <kulachokn@gmail.com>
2025-01-08 16:41:13 +0100

    task 7

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,19 @@
-// Write your code here
+function getStudents(classroom) {
+  let { hasTeachingAssistant, classList } = classroom;
+  let teacher, teachingAssistant, students;
+
+  if (hasTeachingAssistant) {
+    [teacher, teachingAssistant, ...students] = classList;
+  } else {
+    [teacher, ...students] = classList;
+  }
+
+  return students;
+}
+
+const obj = {
+  hasTeachingAssistant: true,
+  classList: ["Rad", "John", "Roman", "Lisa", "Otto", "Luke"],
+};
+
+console.log(getStudents(obj));
