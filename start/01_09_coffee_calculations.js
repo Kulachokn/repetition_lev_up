59a27d5d3a0da6fa1b51dec5492d572973d8b309
Anastasiia Kulachok kulachokn@gmail.com <kulachokn@gmail.com>
2025-01-13 15:58:14 +0100

    task 9

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,10 @@
-// Write your code here
+const price = 1.25;
+
+function sum(arr) {
+  let total = arr.reduce((acc, cur) => {
+    return acc + cur * price;
+  }, 0);
+
+  return `The total bill is $${total}`;
+}
+console.log(sum([2, 3, 1, 5]));
