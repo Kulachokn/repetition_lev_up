e0b78b923d7bfcac5d522d96858426fca391df9d
Anastasiia Kulachok kulachokn@gmail.com <kulachokn@gmail.com>
2025-01-19 14:36:31 +0100

    task 12

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,20 @@
-// Write your code here
+// function numPermutations(word) {
+//   let total = 1; // let total;
+//   for (let i = 1; i <= word.length; i += 1) {
+//     // if (total === undefined) {
+//     //   total = i;
+//     //   continue;
+//     // }
+//     total *= i;
+//   }
+//   return total;
+// }
+
+function numPermutations(word) {
+  if (word.length === 1) {
+    return 1;
+  }
+  return word.length * numPermutations(word.slice(1));
+}
+
+console.log(numPermutations("hello"));
