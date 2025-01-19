e2700ca45e4abb043fe6b55b5e6e8de72e8301ec
Anastasiia Kulachok kulachokn@gmail.com <kulachokn@gmail.com>
2025-01-19 19:45:45 +0100

    task 15

--- start/01_15_password_strength.js
+++ start/01_15_password_strength.js
@@ -1,1 +1,18 @@
-// Write your code here
+function checkPassword(password) {
+  let oneLowerCaseLetter = /(?=.+[a-z])/;
+  let oneUpperCaseLetter = /(?=.+[A-Z])/;
+  let oneDigit = /(?=.+[0-9])/;
+  let oneSpecialCharacter = /(?=.+[!@#$%^&*])/;
+  let minimumEightCharacters = /(?=.{8,})/;
+  let isValid =
+    oneLowerCaseLetter.test(password) &&
+    oneUpperCaseLetter.test(password) &&
+    oneDigit.test(password) &&
+    oneSpecialCharacter.test(password) &&
+    minimumEightCharacters.test(password);
+
+  return isValid ? "Your password is valid" : "Your password is invalid";
+}
+
+console.log(checkPassword("sfdxtrtyf5"));
+console.log(checkPassword("hfA5!ht865"));
