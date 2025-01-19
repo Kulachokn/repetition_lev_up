683fe0d012b2f709b1c5cf769eeef7141cc511c8
Anastasiia Kulachok kulachokn@gmail.com <kulachokn@gmail.com>
2025-01-19 17:30:21 +0100

    task 14

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,10 @@
-// Write your code here
+function urLify(string) {
+  return string
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9\s]/g, "")
+    .split(" ")
+    .join("-");
+}
+
+console.log(urLify("My blog name!"));
+console.log(urLify("Anna's blog"));
